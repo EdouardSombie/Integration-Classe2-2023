50bea28164364282e185c9cd29ebfa65a008eeee
Edouard <edouard@voltajazz.com>
2024-01-25 10:01:58 +0100

    Début avant dernière séance (projet vite js)

--- Framework - vite/Framework/src/js/components/navbar.js
+++ Framework - vite/Framework/src/js/components/navbar.js
@@ -1,1 +1,10 @@
 /* script du composant navbar */
+window.addEventListener("load", () => {
+	document.querySelectorAll(".navbar_toggle_button").forEach((elem) => {
+		console.log(elem);
+		elem.onclick = () => {
+			const targetName = elem.getAttribute("data-target");
+			document.querySelector(targetName).classList.toggle("toggled");
+		};
+	});
+});
